1a9adc8fa3dda47c7b79d83784d6240b62e01072
jagnani73 <yashjagnani73@gmail.com>
2022-01-11 15:50:55 +0530

    feat: upgrade to latest versions

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: "John Doe" });
 }
